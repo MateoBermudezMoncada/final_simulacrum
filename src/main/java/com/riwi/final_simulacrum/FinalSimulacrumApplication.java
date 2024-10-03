bad48fa9bb3ca9edf88471fc8ae7bc0069a558c1
EdgarC97 <ecardona742@gmail.com>
2024-10-03 14:25:01 -0500

    chore: initial commit

---
+++ src/main/java/com/riwi/final_simulacrum/FinalSimulacrumApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.final_simulacrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSimulacrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalSimulacrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/final_simulacrum/FinalSimulacrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.final_simulacrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSimulacrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
